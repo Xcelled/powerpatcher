ff617a5d34f0632e4520b57c05523cb5ad86ee52
Xcelled194 <j12@ketelaar.com>
2014-02-13 23:54:36 -0500

    Fixed PP not being able to find Mabi when installed via steam

--- PowerPatcher/Program.cs
+++ PowerPatcher/Program.cs
@@ -80,29 +80,33 @@ namespace PowerPatcher
 
 		private static void CheckNexonDir()
 		{
-			var nxdir = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_CURRENT_USER\Software\Nexon\Mabinogi", null, "");
-			Logger.Info("Mabinogi directory: {0}", nxdir);
-
-			if (string.IsNullOrWhiteSpace(nxdir))
+			if (Properties.Settings.Default.WarnIfNotInMabiDir)
 			{
-				MessageBox.Show("Oh dear. It appears bandits have made off with your Mabinogi, or Mabinogi isn't installed on this computer.\r\n\r\nPower Patcher is NOT an installer... ;)\r\n\r\nPower Patcher will exit.", "Dem bandits!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-				Logger.Error("Nexon registry key not found.");
-				Application.Exit();
-				Environment.Exit(0);
-			}
+				var nxdir = (string)Microsoft.Win32.Registry.GetValue(@"HKEY_CURRENT_USER\Software\Nexon\Mabinogi", null, "");
+				Logger.Info("Mabinogi directory: {0}", nxdir);
+
+				if (string.IsNullOrWhiteSpace(nxdir))
+				{
+					MessageBox.Show("Oh dear. It appears bandits have made off with your Mabinogi, or Mabinogi isn't installed on this computer.\r\n\r\n"
+					+ " Power Patcher is NOT an installer... ;) If you're trying to install Mabinogi, you should exit and use an official installer.\r\n\r\n"
+					+ "It is also possible that Mabinogi is installed but in an unusual place. If this is the case, feel free to ignore this message.", "Dem bandits!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					Logger.Error("Nexon registry key not found.");
+					return;
+				}
 
 #if !DEBUG
-			if (Properties.Settings.Default.WarnIfNotInMabiDir && (!string.Equals(Path.GetFullPath(nxdir), Path.GetFullPath(Environment.CurrentDirectory), StringComparison.OrdinalIgnoreCase)))
-			{
-				Logger.Warning("Power Patcher is not located in the Mabinogi folder!");
-				if (MessageBox.Show("Power Patcher is not located in the Mabinogi folder (" + nxdir + ")\r\n\r\nThis will most likely result in improper operation. Please restore Power Patcher to the Mabinogi directory as soon as possible. " +
-					"\r\n\r\nPress \"Yes\" to continue (not reccommended!) or \"No\" to exit.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+				if (!string.Equals(Path.GetFullPath(nxdir), Path.GetFullPath(Environment.CurrentDirectory), StringComparison.OrdinalIgnoreCase))
 				{
-					Application.Exit();
-					Environment.Exit(0);
+					Logger.Warning("Power Patcher is not located in the Mabinogi folder!");
+					if (MessageBox.Show("Power Patcher is not located in the Mabinogi folder (" + nxdir + ")\r\n\r\nThis will most likely result in improper operation. Please restore Power Patcher to the Mabinogi directory as soon as possible. " +
+						"\r\n\r\nPress \"Yes\" to continue (not reccommended!) or \"No\" to exit.", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+					{
+						Application.Exit();
+						Environment.Exit(0);
+					}
 				}
-			}
 #endif
+			}
 		}
 
 		private static void SetCWD()
